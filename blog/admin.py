1ac68f697aeb25236944f1c2ceea479f4a8716b9
Marinho Brandao <marinho@gmail.com>
2011-11-02 07:41:08 -0200

    Issue #1 - Unable to change the slug when a blog post is edited

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from models import Post
 class ModulePost(admin.CrudModule):
     model = Post
     list_display = ('slug', 'name','date', 'text')
-    readonly_fields = ('slug','date')
+    readonly_fields = ('date',)
 
 class AppBlog(admin.AdminApplication):
     title = 'Blog'
