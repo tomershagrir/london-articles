eff0811e8512e8d069ebc42230f02fee67a33958
Bruno Gola <brunogola@gmail.com>
2011-11-17 13:52:25 -0200

    sites context processor

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,7 +1,8 @@
-from london.conf import settings
+from london.apps.sites.models import Site
 
 from blog.views import is_writer 
 
 def basic(request):
-    return {'writer': is_writer(request.user)}
+    return {'writer': is_writer(request.user),
+            'sites': Site.query().filter(is_active=True)}
 
