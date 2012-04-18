bd10087c568313bc073540f4bd6c6d64e1baee88
Bisibuka <bisibuka@gmail.com>
2012-04-18 17:27:16 +0300

    images integrated into posts

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from london.apps import admin
 
 from models import Post
+from forms import PostForm
 
 class ModulePost(admin.CrudModule):
     model = Post
     list_display = ('slug', 'name','date', 'text')
     readonly_fields = ('date', 'text')
+    form = PostForm
 
 class AppBlog(admin.AdminApplication):
     title = 'Blog'
