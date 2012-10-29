4f930e00f0af7ab84c780b98167e79203150c51d
bisibuka <bisibuka@gmail.com>
2012-10-29 18:00:37 +0200

    small adjustment to avoid possible error

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -3,6 +3,6 @@ from london.apps.sites.models import Site
 from articles.views import is_writer
 
 def basic(request):
-    return {'writer': is_writer(request.user),
+    return {'writer': is_writer(getattr(request, 'user', None)),
             'sites': Site.query().filter(is_active=True)}
 
